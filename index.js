d19c6b1ecf4ded4d4e7c175a23e25152fd6f031c
liping marlow <sabrina0713@users.noreply.github.com>
2019-08-02 09:16:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const io = require('socket.io')(server,{
+  perMessageDeflate :false
+});
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
